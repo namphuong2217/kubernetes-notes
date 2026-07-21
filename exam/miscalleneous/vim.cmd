7b503a7c001ca1f20737a0fea70a1d04c0e59c8a
nam-phuong-nguyen-adesso-de <nam-phuong.nguyen@adesso.de>
2026-07-21 18:36:29 +0200

    Refactor

--- exam/miscalleneous/vim.cmd
+++ exam/miscalleneous/vim.cmd
@@ -1,11 +1,3 @@
-:wq! # write and quit
-:q! # quit
- # escape
-
-vi q1.yaml
-
-:q!
-
 In Vim, after you delete a line, you are still in Normal mode. You can’t type text yet.
 
 To start typing again, you need to switch into Insert mode:
